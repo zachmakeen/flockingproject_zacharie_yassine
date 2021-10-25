8f3a9fe485c75888132d6bcf82a842cc660c2e12
Zacharie Makeen <zach.makeen@gmail.com>
2021-10-24 21:01:13 -0400

    mend

--- FlockingBackend/Vector2.cs
+++ FlockingBackend/Vector2.cs
@@ -12,5 +12,10 @@ namespace FlockingBackend
 
         public float Vx { get; }
         public float Vy { get; }
+
+        public static Vector2 operator +(Vector2 u, Vector2 v) => new Vector2(u.Vx + v.Vx, u.Vy + v.Vy);
+        public static Vector2 operator -(Vector2 u, Vector2 v) => new Vector2(u.Vx - v.Vx, u.Vy - v.Vy);
+        public static Vector2 operator /(Vector2 u, float f) => new Vector2(u.Vx / f, u.Vy / f);
+        public static Vector2 operator *(Vector2 u, float f) => new Vector2(u.Vx * f, u.Vy * f);
     }
 }
