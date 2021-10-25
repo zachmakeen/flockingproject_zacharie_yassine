0d18ee4945c0b1cdd93a1e76b2d86aabc4fcb793
Zacharie Makeen <zach.makeen@gmail.com>
2021-10-24 21:05:21 -0400

    Add formula methods.

--- FlockingBackend/Vector2.cs
+++ FlockingBackend/Vector2.cs
@@ -17,5 +17,12 @@ namespace FlockingBackend
         public static Vector2 operator -(Vector2 u, Vector2 v) => new Vector2(u.Vx - v.Vx, u.Vy - v.Vy);
         public static Vector2 operator /(Vector2 u, float f) => new Vector2(u.Vx / f, u.Vy / f);
         public static Vector2 operator *(Vector2 u, float f) => new Vector2(u.Vx * f, u.Vy * f);
+
+        public static float DistanceSquared(Vector2 u, Vector2 v) => (float) (Math.Pow(u.Vx - v.Vx, 2) + Math.Pow(u.Vy - v.Vy, 2));
+        public static Vector2 Normalize(Vector2 u)
+        {
+            float magnitude = (float) Math.Sqrt(Math.Pow(u.Vx, 2) + Math.Pow(u.Vy, 2));
+            return new Vector2(u.Vx / magnitude, u.Vy / magnitude);
+        }
     }
 }
