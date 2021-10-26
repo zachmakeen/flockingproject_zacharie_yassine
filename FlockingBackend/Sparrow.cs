567d0d43941bdcf03c6ee44401b5842031020a84
yassine ibhir <yibhir101@gmail.com>
2021-10-26 06:51:12 -0400

    add delegates class

---
+++ FlockingBackend/Sparrow.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace FlockingBackend
+{
+    ///<summary>
+    ///This class is used to represent a single sparrow. 
+    ///This class is just a starting point. Complete the TODO sections
+    ///</summary>
+    public class Sparrow
+    {
+      
+    }
+}
