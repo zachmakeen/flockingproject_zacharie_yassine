5d2a061d4cb27f5223e7627b360db7c17523ca6f
yassine ibhir <yibhir101@gmail.com>
2021-10-27 16:47:14 -0400

    add Flock.cs implementation

--- FlockingBackend/Flock.cs
+++ FlockingBackend/Flock.cs
@@ -9,20 +9,44 @@ namespace FlockingBackend
     ///</summary>
     public class Flock
     {
-        //TODO: Add the events. See instructions for details
 
-        //TODO: Add a Subscribe method that takes as input a CalculateMoveVector delegate instance, 
-        //a MoveBird delegate instance and an optional CalculateRavenAvoidance delegate instance and
-        // subscribes them to the corresponding events
+        ///<value> event <c>CalcMovementEvent</c> Event to calculate movement for each Sparrow/Raven</value>
+        private event Delegates.CalculateMoveVector CalcMovementEvent;
+
+        ///<value> event <c>MoveEvent</c> Event to move Sparrow/Raven</value>
+        private event Delegates.MoveBird  MoveEvent;
+
+        ///<value> event <c>calcRavenFleeEven</c> Event to calculate the vector amountToSteer to flee the chasing Raven</value>
+        private event Delegates.CalculateRavenAvoidance  CalcRavenFleeEvent;
 
         ///<summary>
         ///This method raises the calculate and move events
         ///</summary>
         ///<param name="sparrows">List of Sparrow objects</param>
         ///<param name="raven">A Raven object</param>
+
         public void RaiseMoveEvents(List<Sparrow> sparrows, Raven raven)
         {
-            //TODO
+            CalcMovementEvent?.Invoke(sparrows);
+            MoveEvent?.Invoke();
+            CalcRavenFleeEvent?.Invoke(raven);
+
+        }
+
+        ///<summary>
+        ///This method subscribes the sparrows and raven to the events. 
+        ///</summary>
+        ///<param name="calculateMoveVector">CalculateMoveVector delegate instance</param>
+        ///<param name="moveBird">MoveBird delegate instance</param>
+        ///<param name="calculateRavenAvoidance">optional CalculateRavenAvoidance delegate instance for sparrows</param>
+        public void Subscribe(Delegates.CalculateMoveVector calculateMoveVector, Delegates.MoveBird moveBird, Delegates.CalculateRavenAvoidance calculateRavenAvoidance = default)
+        {
+            CalcMovementEvent += calculateMoveVector;
+
+            MoveEvent += moveBird;
+
+            CalcRavenFleeEvent += calculateRavenAvoidance;
+
         }
 
         
