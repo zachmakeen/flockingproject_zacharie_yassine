7e238cdb0c3d8cca1d3b6d9a24854ac50a929131
yassine ibhir <yibhir101@gmail.com>
2021-10-28 14:15:10 -0400

    add test for events when they are null

---
+++ FlockingUnitTests/FlockSubscribeTest.cs
@@ -0,0 +1,29 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlockingBackend;
+using System.Collections.Generic;
+namespace FlockingUnitTests
+{
+    [TestClass]
+    public class FlockSubscribeTest
+    {
+
+        // Test if event dont't throw exceptions when they are null
+        [TestMethod]
+        public void TestEventNull()
+        {
+
+            Flock flock = new Flock();
+
+            List<Sparrow> sparrows = new List<Sparrow>();
+            sparrows.Add(new Sparrow());
+            Raven raven = new Raven();
+
+            flock.RaiseMoveEvents(sparrows,raven);
+
+        }
+
+        
+    }
+}
+
