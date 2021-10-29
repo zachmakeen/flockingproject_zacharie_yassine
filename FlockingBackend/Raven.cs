1f328e48acfb848cb1cec2dcc70cf8c1b91f6434
Yassine Ibhir <yassine.ibhir@dawsoncollege.qc.ca>
2021-10-29 12:10:53 -0400

    resolve conflict

--- FlockingBackend/Raven.cs
+++ FlockingBackend/Raven.cs
@@ -5,30 +5,6 @@ namespace FlockingBackend
 {
     ///<summary>
     ///This class is used to represent a single raven. 
-<<<<<<< HEAD
-    ///This class is just a starting point. Complete the TODO sections
-    ///</summary>
-    public class Raven
-    {
-        //TODO: Add the constructor, properties and fields as specified in the instructions document.
-
-        ///<value> Property <c>Rotation</c> to rotate the raven to face the direction it is moving toward.</value>
-        public float Rotation
-        {
-            get 
-            {
-                return (float)Math.Atan2(this.velocity.Y, this.velocity.X); 
-            }
-        }
-
-        ///<summary>
-        ///This method is an event handler that updates the velocity and position of a raven.
-        ///</summary>
-        public void Move()
-        {
-            //TODO: 
-        }
-=======
     ///</summary>
     public class Raven : Bird
     {
@@ -54,53 +30,20 @@ namespace FlockingBackend
             //
         }
 
->>>>>>> c4d503614c88f4df2251c9bcc3b97109e47c9bda
         ///<summary>
         ///This method is an event handler to calculate and set amountToSteer vector
         ///</summary>
         ///<param name="sparrows">List of sparrows</param>
-<<<<<<< HEAD
-        public void CalculateBehaviour(List<Sparrow> sparrows) 
-=======
         public override void CalculateBehaviour(List<Sparrow> sparrows) 
->>>>>>> c4d503614c88f4df2251c9bcc3b97109e47c9bda
         {
             //TODO: Set the amountToSteer vector with the vector returned by the ChaseSparrow.
         }
 
         //TODO: Code the following private helper methods to implement chase sparrows.
         //The method header are declared below:
-<<<<<<< HEAD
-        private Vector2 ChaseSparrow (List<Sparrow> sparrows);
-        
-       
-       ///<summary>
-        ///This method is a private helper method to make sparrows reappear on the opposite edge if they go outside the bounds of the screen
-        ///</summary>
-       private void AppearOnOppositeSide()
-       {
-           if (this.Position.X > World.Width)
-            {
-                this.Position = new Vector2(0, this.Position.Y);
-            }
-            else if(this.Position.X < 0)
-            {
-                 this.Position = new Vector2(World.Width, this.Position.Y);
-            }
-            if (this.Position.Y > World.Height)
-            {
-                this.Position = new Vector2(this.Position.X, 0);
-            }
-            else if(this.position.Y < 0)
-            {
-                this.Position= new Vector2(this.Position.X, World.Height);
-            }
-       }
-=======
         private Vector2 ChaseSparrow (List<Sparrow> sparrows)
         {
             return new Vector2(0, 0);
         }
->>>>>>> c4d503614c88f4df2251c9bcc3b97109e47c9bda
     }
 }
--- FlockingBackend/Sparrow.cs
+++ FlockingBackend/Sparrow.cs
@@ -5,30 +5,6 @@ namespace FlockingBackend
 {
     ///<summary>
     ///This class is used to represent a single sparrow. 
-<<<<<<< HEAD
-    ///This class is just a starting point. Complete the TODO sections
-    ///</summary>
-    public class Sparrow
-    {
-        //TODO: Add the constructor, properties and fields as specified in the instructions document.
-
-        ///<value> Property <c>Rotation</c> to rotate the Sparrow to face the direction it is moving toward.</value>
-        public float Rotation
-        {
-            get 
-            {
-                return (float)Math.Atan2(this.velocity.Y, this.velocity.X); 
-            }
-        }
-
-        ///<summary>
-        ///This method is an event handler that updates the velocity and position of a sparrow.
-        ///</summary>
-        public void Move()
-        {
-            //TODO: 
-        }
-=======
     ///</summary>
     public class Sparrow : Bird
     {
@@ -54,18 +30,10 @@ namespace FlockingBackend
             //
         }
 
->>>>>>> c4d503614c88f4df2251c9bcc3b97109e47c9bda
         ///<summary>
         ///This method is an event handler to calculate and set amountToSteer vector using the flocking algorithm rules
         ///</summary>
         ///<param name="sparrows">List of sparrows</param>
-<<<<<<< HEAD
-        public void CalculateBehaviour(List<Sparrow> sparrows) 
-        {
-            //TODO: Set the amountToSteer vector with the vectors returned by 
-            //Cohesion, Alignment, Avoidance methods
-        }
-=======
         public override void CalculateBehaviour(List<Sparrow> sparrows) 
         {
             //TODO: Set the amountToSteer vector with the vectors returned by 
@@ -73,53 +41,17 @@ namespace FlockingBackend
             
         }
 
->>>>>>> c4d503614c88f4df2251c9bcc3b97109e47c9bda
         ///<summary>
         ///This method is an event handler to calculate and update amountToSteer vector with the amount to steer to flee a chasing raven
         ///</summary>
         ///<param name="raven">A Raven object</param>
         public void CalculateRavenAvoidance(Raven raven)
         {
-<<<<<<< HEAD
-             //TODO: Add the vector returned by FleeRaven to the amountToSteer vector.
-=======
             
->>>>>>> c4d503614c88f4df2251c9bcc3b97109e47c9bda
         }
 
         //TODO: Code the following private helper methods to implement the flocking algorithm rules. 
         //The method headers are declared below:
-<<<<<<< HEAD
-        private Vector2 Alignment (List<Sparrow> sparrows);
-        private Vector2 Cohesion (List<Sparrow> sparrows);
-        private Vector2 Avoidance (List<Sparrow> sparrows);
-        private Vector2 FleeRaven(Raven raven);
-        
-       
-       ///<summary>
-        ///This method is a private helper method to make sparrows reappear on the opposite edge if they go outside the bounds of the screen
-        ///</summary>
-       private void AppearOnOppositeSide()
-       {
-    
-           if (this.Position.X > World.Width)
-            {
-                this.Position = new Vector2(0, this.Position.Y);
-            }
-            else if(this.Position.X < 0)
-            {
-                 this.Position = new Vector2(World.Width, this.Position.Y);
-            }
-            if (this.Position.Y > World.Height)
-            {
-                this.Position = new Vector2(this.Position.X, 0);
-            }
-            else if(this.position.Y < 0)
-            {
-                this.Position= new Vector2(this.Position.X, World.Height);
-            }
-       }
-=======
         private Vector2 Alignment (List<Sparrow> sparrows)
         {
             return new Vector2(0, 0);
@@ -137,6 +69,5 @@ namespace FlockingBackend
         {
             return new Vector2(0, 0);
         }
->>>>>>> c4d503614c88f4df2251c9bcc3b97109e47c9bda
     }
 }
